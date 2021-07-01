b94c8a1db7d91221829d49b5d4f7032a552b718d
outlaw6 <milan.damjanovic.mbs@gmail.com>
2021-07-01 17:25:42 +0200

    New procs

---
+++ scroll js/app.js
@@ -0,0 +1,16 @@
+const date = document.getElementById('date');
+date.innerHTML = new Date().getFullYear();
+
+
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', function() {
+	linksContainer.classList.toggle('show-links');
+	const containerHeight = linksContainer.getBoundingClientRect();
+	console.log(containerHeight);
+});
+
+
