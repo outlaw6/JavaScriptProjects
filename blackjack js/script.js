504111c9d2b684d58d124c6c561d09fef191a11f
outlaw6 <milan.damjanovic.mbs@gmail.com>
2021-07-05 10:19:35 +0200

    BJ Logic

--- blackjack js/script.js
+++ blackjack js/script.js
@@ -2,15 +2,13 @@
 function blackjackHit() {
 	alert('auhc');
 }
-function blackjackDeal() {
-		let yourImages = document.querySelector('#your-box').querySelectorAll('img');
-		console.log(yourImages);
 
-}
 
 let blackjackGame = {
 	'you': {   'scoreSpan': '#your-blackjack-result',   'div': '#your-box', 'score': 0},
-	'dealer': {'scoreSpan': '#dealer-blackjack-result', 'div': '#dealer-box', 'score': 0}
+	'dealer': {'scoreSpan': '#dealer-blackjack-result', 'div': '#dealer-box', 'score': 0},
+	'cards': ['1', '2', '3', '4', '6', '7', '8', '9', '10', 'K', 'J', 'Q', 'A'],
+	'cardsMap': {'2': 2, '3': 3, '4': 4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'K': 10, 'J':10, 'Q':10, 'A': [1, 11]}
 };
 
 let a = document.querySelector('#blackjack-hit-button')
@@ -24,17 +22,39 @@ const DEALER = blackjackGame['dealer'];
 const hitSound = new Audio('sounds/swish.m4a');
 
 function blackjackHit() {
-	showCard(DEALER);
+	let card = randomCard();
+	console.log(card);
+	showCard(DEALER, card	);
+}
+function randomCard(){
+	let randomIndx = Math.floor(Math.random() * 13);
+	return blackjackGame['cards'][randomIndx];
 }
 
 
-function showCard(activePlayer) {
+function showCard(activePlayer, card) {
 	let cardImage = document.createElement('img');
-	cardImage.src = 'images/Q.png';
+	// cardImage.src = 'images/'+ randomCard() +'.png';
+	cardImage.src = `images/${card}.png`;
 	document.querySelector(activePlayer['div']).appendChild(cardImage);
 	hitSound.play();
 
 }
+
+function blackjackDeal() {
+		let yourImages = document.querySelector('#your-box').querySelectorAll('img');
+
+		let dealerImages = document.querySelector('#dealer-box').querySelectorAll('img');
+		console.log(yourImages);
+
+		for (let a=0;a<yourImages.length;a++) {
+			yourImages[a].remove();
+		}
+		for (let a=0;a<dealerImages.length;a++) {
+			dealerImages[a].remove();
+		}
+}
+
 
 
 //black jack js
