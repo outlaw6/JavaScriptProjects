ecf43ccd08fd9deaa7e7440465f87e6dc1c68b48
outlaw6 <milan.damjanovic.mbs@gmail.com>
2021-07-05 09:50:37 +0200

    BJ Logic

--- blackjack js/script.js
+++ blackjack js/script.js
@@ -1,24 +1,40 @@
-let a = document.querySelector('#blackjack-hit-button')
-a.addEventListener('click', blackjackHit);
-
 
 function blackjackHit() {
 	alert('auhc');
 }
+function blackjackDeal() {
+		let yourImages = document.querySelector('#your-box').querySelectorAll('img');
+		console.log(yourImages);
+
+}
 
 let blackjackGame = {
-	'you': {'scoreSpan': '#your-blackjack-result', 'div': '#your-box', 'score': 0},
+	'you': {   'scoreSpan': '#your-blackjack-result',   'div': '#your-box', 'score': 0},
 	'dealer': {'scoreSpan': '#dealer-blackjack-result', 'div': '#dealer-box', 'score': 0}
 };
+
+let a = document.querySelector('#blackjack-hit-button')
+a.addEventListener('click', blackjackHit);
+
+document.querySelector('#blackjack-deal-button').addEventListener('click', blackjackDeal);		
 
 
 const YOU = blackjackGame['you'];
 const DEALER = blackjackGame['dealer'];
+const hitSound = new Audio('sounds/swish.m4a');
 
 function blackjackHit() {
+	showCard(DEALER);
+}
+
+
+function showCard(activePlayer) {
 	let cardImage = document.createElement('img');
 	cardImage.src = 'images/Q.png';
-	document.querySelector(YOU['div']).appendChild(cardImage);
+	document.querySelector(activePlayer['div']).appendChild(cardImage);
+	hitSound.play();
 
 }
 
+
+//black jack js
